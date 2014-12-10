8ba3e19e1f2f5e1dcd8c525f207af2541c563607
AnilU <anilugale1989@gmail.com>
2014-12-10 16:15:25 +0530

    some chnage

--- app/src/main/java/com/intellvisoin/myapplication/MainActivity2.java
+++ app/src/main/java/com/intellvisoin/myapplication/MainActivity2.java
@@ -1,9 +1,10 @@
 package com.intellvisoin.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity2 extends ActionBarActivity {
@@ -12,6 +13,7 @@ public class MainActivity2 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2);
+        TextView text=new TextView(this);
     }
 
 
